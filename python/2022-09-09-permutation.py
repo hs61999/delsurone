e024ed4af65455f88c820b3c0f04e818f58ff416
hs61999 <hs61999@users.noreply.github.com>
2022-09-24 16:00:54 +0000

    deploy: 1224435d4a4746be57546eaa639876eeefcceff4

--- python/2022-09-09-permutation.py
+++ python/2022-09-09-permutation.py
@@ -107,7 +107,7 @@ def word_connect(string, start_num, end_num):
 
 if __name__ == "__main__":
     
-    string = "dewsay"    
+    string = "urcedv"    
     
     start_time = time.perf_counter()
     word_connect(string, 3, len(string))
